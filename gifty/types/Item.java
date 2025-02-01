e5933e6dd9ef8db98f6dbca939bea276444bbfe7
Eslam Ahmed <eslam.ahmed930950@gmail.com>
2025-02-01 21:22:24 +0200

    Added class item attributes

--- gifty/types/Item.java
+++ gifty/types/Item.java
@@ -1,5 +1,9 @@
 package gifty.types;
 
 public class Item {
-
+    private int item_id;
+    private String item_name;
+    private String category;
+    private double price;
+    private String status;
 }

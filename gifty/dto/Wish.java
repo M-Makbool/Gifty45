d75400e22b7a22de9c7de103926feef747dd5036
Mohammed Makbool <M.Makbool@outlook.sa>
2025-02-03 21:27:21 +0200

    added constructor and getters for Wish class

--- gifty/dto/Wish.java
+++ gifty/dto/Wish.java
@@ -9,4 +9,25 @@ public class Wish {
     private String status;
     private ArrayList<Conributer> conributUsers;
 
+    public Wish(Item item, Date date, String status) {
+        this.item = item;
+        this.date = date;
+        this.status = status;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public ArrayList<Conributer> getConributUsers() {
+        return conributUsers;
+    }
 }

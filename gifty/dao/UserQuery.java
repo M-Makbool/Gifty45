29fd61484973c9966b41c700ce72bfa12aac7630
Mohammed Makbool <M.Makbool@outlook.sa>
2025-02-04 11:58:29 +0200

    getUsers() added in UserQuery class

--- gifty/dao/UserQuery.java
+++ gifty/dao/UserQuery.java
@@ -10,12 +10,15 @@ public class UserQuery {
     public static ArrayList<UserLogin> getUsers() throws SQLException {
         ResultSet rs;
         try (PreparedStatement pst = DataBase.getConnection()
-                .prepareStatement(" SELECT * FROM items WHERE Status = 'AVAILABLE' ")) {
+                .prepareStatement(" SELECT User_login, user_name FROM users WHERE is_deleted = false; ")) {
             rs = pst.executeQuery();
         }
         ArrayList<UserLogin> users = new ArrayList<>();
-        while (rs.next()) {
-            users.add(new UserLogin());
+        while (rs.next()){
+            users.add(new UserLogin(
+                rs.getString("user_name"),
+                rs.getString("User_login")
+                ));
         }
         return users;
     }

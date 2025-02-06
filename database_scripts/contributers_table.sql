47263ce8cefd6069e9892144539c5ad0fb751f4c
Mohammed Makbool <M.Makbool@outlook.sa>
2025-02-07 01:11:01 +0200

    modified the UserQuery methods and added contribute methods

--- database_scripts/contributers_table.sql
+++ database_scripts/contributers_table.sql
@@ -2,10 +2,10 @@ CREATE TABLE Contributers (
     Item_id NUMBER,
     User_login VARCHAR2(50),
     Wish_Date DATE,
-    Contributee_id NUMBER,
+    Contributee_login VARCHAR2(50),
     Contribution_date DATE,
     Contribution_amount NUMBER(10,2),
-    PRIMARY KEY (Item_id, User_login, Wish_Date, Contributee_id, Contribution_date),
+    PRIMARY KEY (Item_id, User_login, Wish_Date, Contributee_login, Contribution_date),
     FOREIGN KEY (Item_id) REFERENCES Items(Item_id),
     FOREIGN KEY (User_login) REFERENCES Users(User_login)
 );
--- gifty/dao/UserQuery.java
+++ gifty/dao/UserQuery.java
@@ -5,186 +5,236 @@ import gifty.dto.Item;
 import gifty.dto.User;
 import gifty.dto.UserLogin;
 import gifty.dto.Wish;
-import java.sql.Connection;
 import java.sql.Date;
-import oracle.sql.DATE;
-
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class UserQuery {
+
     public static ArrayList<UserLogin> getUsers() throws SQLException {
-        ResultSet rs;
-        try (PreparedStatement pst = DataBase.getConnection()
-                .prepareStatement(" SELECT User_login, user_name FROM users WHERE is_deleted = false; ")) {
-            rs = pst.executeQuery();
+
+        String query = "SELECT User_login, user_name FROM users WHERE is_deleted = false";
+
+        try (PreparedStatement pst = DataBase.getConnection().prepareStatement(query);
+                ResultSet rs = pst.executeQuery()) {
+
+            ArrayList<UserLogin> users = new ArrayList<>();
+            while (rs.next()) {
+                String userName = rs.getString("user_name");
+                String userLogin = rs.getString("User_login");
+                users.add(new UserLogin(userName, userLogin));
+            }
+
+            return users;
         }
-        ArrayList<UserLogin> users = new ArrayList<>();
-        while (rs.next()) {
-            users.add(new UserLogin(
-                    rs.getString("user_name"),
-                    rs.getString("User_login")));
-        }
-        return users;
     }
 
-    public int addUser(User user) throws SQLException {
-        try (PreparedStatement pst = DataBase.getConnection().prepareStatement(
-                " INSERT INTO users (user_login, user_email, user_name, telephone, is_deleted, gender, password, dob) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?)")) {
+    public static int addUser(User user) throws SQLException {
 
+        String query = "INSERT INTO users (user_login, user_email, user_name, telephone, is_deleted, gender, DOB, Password) "
+                + "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+        try (PreparedStatement pst = DataBase.getConnection().prepareStatement(query)) {
             pst.setString(1, user.getLogin());
             pst.setString(2, user.getEmail());
             pst.setString(3, user.getName());
             pst.setString(4, user.getTelephone());
             pst.setBoolean(5, false);
             pst.setString(6, user.getGender());
-            // pst.setInt(7, user.); // we need to handle password
-            pst.setDate(8, user.getDateOfBirth());
-            int rows = pst.executeUpdate();
-            pst.close();
-            DataBase.getConnection().commit();
+            pst.setDate(7, user.getDateOfBirth());
+            pst.setString(8, user.getPassword());
 
-            return rows;
-        }
-    }
-
-    public int deleteUser(UserLogin user) throws SQLException {
-        try (PreparedStatement pst = DataBase.getConnection().prepareStatement(
-                " Update users set is_deleted = true WHERE user_login = ? ")) {
-            pst.setString(1, user.getLogin());
-            int rows = pst.executeUpdate();
-            pst.close();
-            DataBase.getConnection().commit();
-
-            return rows;
-        }
-    }
-
-    public int addFriend(UserLogin user, UserLogin friend) throws SQLException {
-        try (PreparedStatement pst = DataBase.getConnection().prepareStatement(
-                " INSERT INTO friends (user_login, friend_login, status) VALUES (?, ?, 'REQUESTED') ")) {
-            pst.setString(1, user.getLogin());
-            pst.setString(2, friend.getLogin());
-
-            int rows = pst.executeUpdate();
-            pst.close();
-            DataBase.getConnection().commit();
-
-            return rows;
-        }
-    }
-
-    public int removeFriend(UserLogin user, UserLogin friend) throws SQLException {
-        try (PreparedStatement pst = DataBase.getConnection().prepareStatement(
-                " update friends set status = 'Removed' WHERE (user_login = ? AND friend_login = ?) OR (friend_login = ? and user_login = ?) ")) {
-            pst.setString(1, user.getLogin());
-            pst.setString(2, friend.getLogin());
-            pst.setString(3, user.getLogin());
-            pst.setString(4, friend.getLogin());
-            int rows = pst.executeUpdate();
-            pst.close();
-            DataBase.getConnection().commit();
-
-            return rows;
+            return pst.executeUpdate();
         }
     }
 
     public static User getUser(UserLogin userlogin) throws SQLException {
-        ResultSet rs;
-        PreparedStatement pst = DataBase.getConnection().prepareStatement(
-                " select * from users where user_login = ? and status = 'AVAILABLE' ");
-        pst.setString(1, userlogin.getLogin());
-        rs = pst.executeQuery();
-        rs.next();
-        return new User(
-                userlogin,
-                rs.getString("user_email"),
-                rs.getString("Gender"),
-                rs.getString("telephone"),
-                rs.getDate("DOB")
 
-        );
+        try (PreparedStatement pst = DataBase.getConnection().prepareStatement(
+                " select * from users where user_login = ? and Is_Deleted = false ")) {
+            pst.setString(1, userlogin.getLogin());
+            User user;
+            try (ResultSet rs = pst.executeQuery()) {
+                rs.next();
+                user = new User(new UserLogin(rs.getString("user_name"), userlogin.getLogin()),
+                        rs.getString("user_email"), rs.getString("Gender"),
+                        rs.getString("telephone"), rs.getDate("DOB"));
+            }
+            user.setFriendList(new ArrayList<>(getFriends(user)));
+            user.setWishList(new ArrayList<>(getWishList(user)));
+            return user;
+        }
     }
 
-    public static int deleteWish(UserLogin userlogin, Wish wish) throws SQLException {
-        PreparedStatement pst = DataBase.getConnection().prepareStatement(
-                " UPDATE Wishing_list SET status = 'DELETED' WHERE user_login = ? AND Item_id = ? ");
-        pst.setString(1, userlogin.getLogin());
-        pst.setInt(2, wish.getItem().getItem_id());
-        return pst.executeUpdate();
+    public static int deleteUser(UserLogin user) throws SQLException {
+
+        try (PreparedStatement pst = DataBase.getConnection().prepareStatement(
+                " Update users set is_deleted = true WHERE user_login = ? AND Password = ? ")) {
+
+            pst.setString(1, user.getLogin());
+            pst.setString(2, user.getPassword());
+
+            return pst.executeUpdate();
+        }
     }
 
-    public static int addWish(UserLogin userlogin, Item item) throws SQLException {
-        PreparedStatement pst = DataBase.getConnection().prepareStatement(
-                " INSERT INTO Wishing_list (Item_id,User_login,Wish_Date ,Status) VALUES (?, ? ,? , 'ACTIVE') ");
-        pst.setInt(1, item.getItem_id());
-        pst.setString(2, userlogin.getLogin());
-        pst.setDate(3, DATE.getCurrentDate().dateValue());
-        return pst.executeUpdate();
+    public static int addFriend(UserLogin user, UserLogin friend) throws SQLException {
 
+        try (PreparedStatement pst = DataBase.getConnection().prepareStatement(
+                " INSERT INTO friends (user_login, friend_login, status) VALUES (?, ?, 'REQUESTED') ")) {
+
+            pst.setString(1, user.getLogin());
+            pst.setString(2, friend.getLogin());
+
+            return pst.executeUpdate();
+        }
+    }
+
+    public static int acceptFriend(UserLogin user, UserLogin friend) throws SQLException {
+
+        String query = "UPDATE friends SET status = 'ACCEPTED' "
+                + "WHERE user_login = ? AND friend_login = ? AND status = 'REQUESTED'";
+
+        try (PreparedStatement pst = DataBase.getConnection().prepareStatement(query)) {
+            pst.setString(1, friend.getLogin());
+            pst.setString(2, user.getLogin());
+
+            return pst.executeUpdate();
+        }
+    }
+
+    public int removeFriend(UserLogin user, UserLogin friend) throws SQLException {
+
+        String query = "UPDATE friends SET status = 'Removed' "
+                + "WHERE (user_login = ? AND friend_login = ?) "
+                + "OR (friend_login = ? AND user_login = ?)";
+
+        try (PreparedStatement pst = DataBase.getConnection().prepareStatement(query)) {
+            pst.setString(1, user.getLogin());
+            pst.setString(2, friend.getLogin());
+            pst.setString(3, user.getLogin());
+            pst.setString(4, friend.getLogin());
+
+            return pst.executeUpdate();
+        }
     }
 
     public static ArrayList<UserLogin> getFriends(UserLogin userLogin) throws SQLException {
-        ArrayList<UserLogin> friends = new ArrayList<>();
-        String query = "select u.User_login , u.user_name from users u join Friends f on u.User_login = f.Friend_login or u.User_login = f.User_login where f.User_login = ? or f.Friend_login = ? and f.Status = 'ACCEPTED'";
-        try (Connection conn = DataBase.getConnection();
-                PreparedStatement pst = conn.prepareStatement(query)) {
+
+        String query = "SELECT u.User_login, u.user_name " + "FROM users u "
+                + "JOIN friends f ON u.User_login = f.Friend_login OR u.User_login = f.User_login "
+                + "WHERE (f.User_login = ? OR f.Friend_login = ?) AND f.Status = 'ACCEPTED'";
+
+        try (PreparedStatement pst = DataBase.getConnection().prepareStatement(query)) {
+
             pst.setString(1, userLogin.getLogin());
+            pst.setString(2, userLogin.getLogin());
+
+            ArrayList<UserLogin> friends = new ArrayList<>();
+
             try (ResultSet rs = pst.executeQuery()) {
                 while (rs.next()) {
-                    friends.add(new UserLogin(rs.getNString("User_name"), rs.getString("User_login")));
+                    friends.add(
+                            new UserLogin(rs.getNString("User_name"), rs.getString("User_login")));
                 }
             }
+            return friends;
         }
-        return friends;
+    }
+
+    public static int addWish(UserLogin userlogin, Item item) throws SQLException {
+
+        try (PreparedStatement pst = DataBase.getConnection().prepareStatement(
+                " INSERT INTO Wishing_list ( Item_id, User_login, Wish_Date, Status ) VALUES ( ?, ?, ?, 'ACTIVE' ) ")) {
+
+            pst.setInt(1, item.getItem_id());
+            pst.setString(2, userlogin.getLogin());
+            pst.setDate(3, new Date(System.currentTimeMillis()));
+
+            return pst.executeUpdate();
+        }
+    }
+
+    public static int deleteWish(UserLogin userlogin, Wish wish) throws SQLException {
+
+        try (PreparedStatement pst = DataBase.getConnection().prepareStatement(
+                " UPDATE Wishing_list SET status = 'DELETED' WHERE user_login = ? AND Item_id = ? ")) {
+
+            pst.setString(1, userlogin.getLogin());
+            pst.setInt(2, wish.getItem().getItem_id());
+
+            return pst.executeUpdate();
+        }
     }
 
     public static ArrayList<Wish> getWishList(UserLogin userLogin) throws SQLException {
-        ArrayList<Wish> wishList = new ArrayList<>();
+
         String query = "select * From wishing_list where User_login = ? ";
-         
-    
-        try (Connection conn = DataBase.getConnection();
-                PreparedStatement pst = conn.prepareStatement(query)) {
+
+        try (PreparedStatement pst = DataBase.getConnection().prepareStatement(query)) {
+
             pst.setString(1, userLogin.getLogin());
+
+            Wish wish;
+            ArrayList<Wish> wishList = new ArrayList<>();
+
             try (ResultSet rs = pst.executeQuery()) {
                 while (rs.next()) {
-                       
-                    wishList.add(new Wish(
-                        rs.getInt("Item_id"),
-                        rs.getDate("Date"),
-                        rs.getString("status"),
-                         ));
+                    wish = new Wish(ItemQuery.getItem(rs.getInt("Item_id")),
+                            rs.getDate("Wish_Date"), rs.getString("status"));
+                    wish.setConributUsers(getContributors(userLogin, wish));
+                    wishList.add(wish);
                 }
             }
+            return wishList;
         }
-        return wishList;
     }
 
-    public static ArrayList<Contributor> getContributors(Wish wish) throws SQLException {
-        ArrayList<Contributor> contributors = new ArrayList<>();
+    public static int contribute(UserLogin friend, UserLogin contributor, Wish wish, double amount)
+            throws SQLException {
 
-        String query = "SELECT user_login, SUM(contribution_amount) AS total_contribution FROM contributions WHERE item_id = ? AND wishing_date = ?  GROUP BY user_login";
+        String query = "INSERT INTO Contributers ( user_login, item_id, wish_date, Contributee_login, Contribution_date, contribution_amount ) "
+                + "VALUES (?, ?, ?, ?, ?, ?)";
 
-        try (Connection conn = DataBase.getConnection();
-                PreparedStatement pst = conn.prepareStatement(query)) {
+        try (PreparedStatement pst = DataBase.getConnection().prepareStatement(query)) {
+            pst.setString(1, friend.getLogin());
+            pst.setInt(2, wish.getItem().getItem_id());
+            pst.setDate(3, wish.getDate());
+            pst.setString(4, contributor.getLogin());
+            pst.setDate(5, new Date(System.currentTimeMillis()));
+            pst.setDouble(6, amount);
+
+            return pst.executeUpdate();
+        }
+    }
+
+    public static ArrayList<Contributor> getContributors(UserLogin userLogin, Wish wish)
+            throws SQLException {
+
+        String query = "SELECT Contributee_login, SUM(contribution_amount) AS total_contribution "
+                + "FROM Contributers WHERE item_id = ? AND Wish_Date = ? AND user_login = ? GROUP BY Contributee_login";
+
+        try (PreparedStatement pst = DataBase.getConnection().prepareStatement(query)) {
 
             pst.setInt(1, wish.getItem().getItem_id());
             pst.setDate(2, wish.getDate());
+            pst.setString(3, userLogin.getLogin());
+
+            ArrayList<Contributor> contributors = new ArrayList<>();
 
             try (ResultSet rs = pst.executeQuery()) {
                 while (rs.next()) {
 
-                    contributors.add(new Contributor(
-                            rs.getString("user_login"),
-                            rs.getDouble("total_contribution")));
+                    contributors.add(
+                            new Contributor(new UserLogin("", rs.getString("Contributee_login")),
+                                    rs.getDouble("total_contribution")));
                 }
             }
+            return contributors;
         }
 
-        return contributors;
     }
 
 }

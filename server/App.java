1aa96a86ab93cc259dcedab41a2bc0622f1bcd2f
Mohammed Makbool <M.Makbool@outlook.sa>
2025-02-10 11:20:50 +0200

    added server login scene with hashed password

--- server/App.java
+++ server/App.java
@@ -1,8 +1,4 @@
-import gifty.dao.*;
-import gifty.dto.*;
 import java.io.IOException;
-import java.sql.Date;
-import java.sql.SQLException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -22,43 +18,9 @@ public class App extends Application {
                 primaryStage.show();
         }
 
-        @SuppressWarnings("CallToPrintStackTrace")
         public static void main(String[] args) {
 
                 launch(args);
-
-                try {
-                        ItemQuery.addItem(new Item(0, "laptop", "devices", 50000, "AVAILABLE"));
-                        ItemQuery.addItem(new Item(0, "Iphone_11", "devices", 20000, "AVAILABLE"));
-                        UserQuery.addUser(new User(new UserLogin("hamada  hamdy  ", "hamada"),
-                                        "hamada@example.com", "Male", "123456789",
-                                        Date.valueOf(java.time.LocalDate.of(2000, 1, 1))));
-                        UserQuery.addUser(new User(
-                                        new UserLogin("  mahmoud abd   elsameea", " mahmoud"),
-                                        "mahmoud@example.com", "Female", "987654321",
-                                        Date.valueOf(java.time.LocalDate.of(2001, 2, 2))));
-                        UserQuery.addWish(UserQuery.getUsers().get(0), ItemQuery.getItems().get(0));
-                        UserQuery.addFriend(UserQuery.getUsers().get(0),
-                                        UserQuery.getUsers().get(1));
-                        UserQuery.acceptFriend(UserQuery.getUsers().get(1),
-                                        UserQuery.getUsers().get(0));
-                        UserQuery.contribute(
-                                        UserQuery.getFriends(UserQuery.getUsers().get(1)).get(0),
-                                        UserQuery.getUsers().get(1),
-                                        UserQuery.getWishList(UserQuery
-                                                        .getFriends(UserQuery.getUsers().get(1))
-                                                        .get(0)).get(0),
-                                        90);
-                        System.out.println(UserQuery.getContributors(
-                                        UserQuery.getFriends(UserQuery.getUsers().get(1)).get(0),
-                                        UserQuery.getWishList(UserQuery
-                                                        .getFriends(UserQuery.getUsers().get(1))
-                                                        .get(0)).get(0))
-                                        .get(0).getLogin());
-                } catch (SQLException e) {
-                        e.printStackTrace();
-                }
-
         }
 
 }
--- server/controllers/ServerLogin.java
+++ server/controllers/ServerLogin.java
@@ -1,9 +1,14 @@
 package controllers;
 
+import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class ServerLogin {
 
@@ -28,6 +33,7 @@ public class ServerLogin {
         }
     }
 
+    @SuppressWarnings("CallToPrintStackTrace")
     public void loginAction() {
 
         String hashedPassword = hash(txtPass.getText());
@@ -35,6 +41,14 @@ public class ServerLogin {
         if (hashedUser.equals("21232f297a57a5a743894a0e4a801fc3")
                 && hashedPassword.equals("21232f297a57a5a743894a0e4a801fc3")) {
             System.out.println("hello server");
+            try {
+                Parent root = FXMLLoader.load(getClass().getResource("../fxmls/ServerHome.fxml"));
+                Stage stage = (Stage) txtPass.getScene().getWindow();
+                stage.setScene(new Scene(root));
+                stage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
